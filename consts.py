da30fa0527e7d7a17da73575e1af4c65a5bbd785
Mert Kırgıl <mert.kirgil@lab2023.com>
2025-05-09 22:34:45 +0300

    Add obstacle images and update sprite paths in consts.py

--- consts.py
+++ consts.py
@@ -6,21 +6,21 @@ PLAYER_SPEED = 5
 OBSTACLE_SPEED = 5
 
 PLAYER_SPRITES_LEFT = [
-    "assets/sprite_left_1.png",
-    "assets/sprite_left_2.png"
+    "assets/heroes/hero_left_down.png",
+    "assets/ heroes/hero_left_up.png"
 ]
 PLAYER_SPRITES_RIGHT = [
-    "assets/sprite_right_1.png",
-    "assets/sprite_right_2.png"
+    "assets/heroes/hero_right_down.png",
+    "assets/heroes/hero_right_up.png"
 ]
 ENGEL_SPRITES = [
-    "assets/engel1.png",
-    "assets/engel2.png",
-    "assets/engel3.png",
-    "assets/engel4.png",
-    "assets/engel5.png",
-    "assets/engel6.png",
-    "assets/engel7.png",
-    "assets/engel8.png",
-    "assets/engel9.png"
+    "assets/obstacles/engel1.png",
+    "assets/obstacles/engel2.png",        
+    "assets/obstacles/engel3.png",
+    "assets/obstacles/engel4.png",
+    "assets/obstacles/engel5.png",
+    "assets/obstacles/engel6.png",
+    "assets/obstacles/engel7.png",
+    "assets/obstacles/engel8.png",
+    "assets/obstacles/engel9.png"
 ]
