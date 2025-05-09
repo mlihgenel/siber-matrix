7cf95ebd94f3ee8ba995cf9b7938d8139a748700
Melih Genel <mlihgenel@gmail.com>
2025-05-09 14:59:38 +0300

    const.py ve game.py oluşturuldu

---
+++ consts.py
@@ -0,0 +1,26 @@
+SCREEN_WIDTH = 650
+SCREEN_HEIGHT = 900
+SCREEN_TITLE = "Siber Matrix"
+
+PLAYER_SPEED = 5
+OBSTACLE_SPEED = 5
+
+PLAYER_SPRITES_LEFT = [
+    "assets/sprite_left_1.png",
+    "assets/sprite_left_2.png"
+]
+PLAYER_SPRITES_RIGHT = [
+    "assets/sprite_right_1.png",
+    "assets/sprite_right_2.png"
+]
+ENGEL_SPRITES = [
+    "assets/engel1.png",
+    "assets/engel2.png",
+    "assets/engel3.png",
+    "assets/engel4.png",
+    "assets/engel5.png",
+    "assets/engel6.png",
+    "assets/engel7.png",
+    "assets/engel8.png",
+    "assets/engel9.png"
+]
---
+++ game.py
@@ -0,0 +1,10 @@
+import arcade
+from consts import SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_TITLE, PLAYER_SPEED, PLAYER_SPRITES_LEFT, PLAYER_SPRITES_RIGHT, ENGEL_SPRITES, OBSTACLE_SPEED
+
+
+class SiberMatrix(arcade.Window):
+    
+    def __init__(self):
+        super.__init__(SCREEN_HEIGHT, SCREEN_WIDTH,SCREEN_TITLE)
+        
+
